8d8bfcf9dceb3ec5956bc7a16490808dc7746bb3
ryko <41332498+miss-ryoko@users.noreply.github.com>
2018-08-03 21:17:02 +0900

    first commit!

---
+++ README.md
@@ -0,0 +1,8 @@
+# misskey-special
+
+This repository is [misskey](https://misskey.xyz) clients for android
+
+Thank you for [Microsoft](https://www.microsoft.com)!
+
+## License
+MIT license
---
+++ app/src/main/java/xyz/misskey/special/MainActivity.java
@@ -0,0 +1,23 @@
+package xyz.misskey.special;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView myWebView = (WebView)findViewById(R.id.mainview);
+
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.getSettings().setDomStorageEnabled(true);
+
+        myWebView.loadUrl("https://misskey.xyz");
+    }
+}
